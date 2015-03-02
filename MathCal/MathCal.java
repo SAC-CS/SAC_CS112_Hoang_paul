829efaafae78df5533a7c2853cc774aa39664d04
SAC_CS112_Hoang_paul <phoang@FJ402019499B038.ochca.com>
2015-03-02 09:19:21 -0800

    first commit

---
+++ MathCal/MathCal.java
@@ -0,0 +1,21 @@
+
+public class MathCal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+System.out.println ("Perfomr Caculation: Add,Subtract, Multiply, Division");
+	// Integer
+   int a = 70;
+   int b = 150;
+   int c = 4;
+   int d = 10;
+   int e = 10;
+   int r = a + b -c * d /e;
+    // Calculate numbers
+   System.out.println ("Result = " +r);
+      
+				
+	}
+
+}
